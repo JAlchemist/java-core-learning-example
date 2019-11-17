7facf69a3e424a6c6bfe559377a480bfe5029f52
yangli <lil.yang@qunar.com>
2019-11-17 18:01:32 +0800

    update

--- src/main/java/com/jaichemist/learning/lambda/LambdaTest.java
+++ src/main/java/com/jaichemist/learning/lambda/LambdaTest.java
@@ -7,11 +7,12 @@ package com.jaichemist.learning.lambda;
 public class LambdaTest {
     public static void main(String[] args) {
         LambdaTest lambdaTest = new LambdaTest();
-
+        //类型声明
         MathOperation addition = (int a, int b) -> a + b;
-
         System.out.println("10 + 5 ="+lambdaTest.operate(10,5,addition));
-
+        //不用类型声明
+        MathOperation subtracation = (a,b) -> a -b;
+        System.out.println("10 - 5 ="+lambdaTest.operate(10,5,subtracation));
     }
 
     interface MathOperation{
